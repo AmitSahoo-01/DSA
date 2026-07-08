a5a9f938c950a2d0ea623db520d41905525a122c
Amit Sahoo <amitkusahoo1212@gmail.com>
2026-07-09 00:28:49 +0530

    adding all codes

---
+++ String/subStrings.java
@@ -0,0 +1,12 @@
+package DSA.String;
+
+public class subStrings {
+    public static void main(String[] args) {
+        String s = "Amit";
+        for (int i =0;i<s.length();i++){
+            for (int j=i+1;j<=s.length();j++){
+                System.out.print(s.substring(i,j)+" ");
+            }
+        }
+    }
+}
