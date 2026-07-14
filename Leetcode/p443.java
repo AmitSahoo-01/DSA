915f7136448abc5a99afaf142d04bdb6f3d3b043
Amit Sahoo <amitkusahoo1212@gmail.com>
2026-07-14 21:01:20 +0530

    adding codes

--- Leetcode/p443.java
+++ Leetcode/p443.java
@@ -2,11 +2,33 @@ package DSA.Leetcode;
 
 public class p443 {
     public static void main(String[] args) {
+        char[] arr = {'a','a','b','b','c','c','d'};
+
+
+        System.out.println(compress(arr));
 
     }
-    public int compress(char[] chars) {
+    public static int compress(char[] chars) {
         int i =0;
         int write = 0;
+        while(i<chars.length){
+            int j = i;
+            while(j<chars.length && chars[i] == chars[j]){
+                j++;
+            }
+            chars[write] = chars[i];
+            write++;
+            int count = j-i;
+            if(count > 1){
+                String cnt = String.valueOf(count);
+                for (char ch : cnt.toCharArray()){
+                    chars[write] = ch;
+                    write++;
+                }
+            }
+            i = j;
 
+        }
+        return write;
     }
 }
